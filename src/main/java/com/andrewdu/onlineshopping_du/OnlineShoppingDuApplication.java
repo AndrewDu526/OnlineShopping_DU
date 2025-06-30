67a0cd333b0abf946d01feda4f1216c2f42ef662
AndrewDu526 <changandu526@gmail.com>
2025-06-29 20:04:43 -0400

    Initial commit

---
+++ src/main/java/com/andrewdu/onlineshopping_du/OnlineShoppingDuApplication.java
@@ -0,0 +1,13 @@
+package com.andrewdu.onlineshopping_du;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingDuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingDuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrewdu/onlineshopping_du/OnlineShoppingDuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewdu.onlineshopping_du;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingDuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
